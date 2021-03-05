631370ea990180c55d169a5831a158d1829c366c
Robert <robertlewiscook@gmail.com>
2021-03-05 13:16:01 -0500

    tables created, select statements added, ready to add users to the database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users , tournaments , games , matches , team;
 DROP SEQUENCE IF EXISTS seq_user_id;
 
 CREATE SEQUENCE seq_user_id
@@ -10,6 +10,7 @@ CREATE SEQUENCE seq_user_id
   CACHE 1;
 
 
+                                                                        --TABLE CREATION 
 CREATE TABLE users (
 	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
 	username varchar(50) NOT NULL,
@@ -23,9 +24,11 @@ CREATE TABLE tournaments(
         start_time              time            NOT NULL,
         start_date              date            NOT NULL,
         number_of_participants  int             NOT NULL,
-        team_id                 varchar(32)     NOT NULL,
+        team_id                 serial          NOT NULL,
         ranking_of_teams        int             NOT NULL,
-        match_id                int             NOT NULL
+        match_id                int             NOT NULL,
+        
+CONSTRAINT       PK_tournament_id     PRIMARY KEY (tournament_id)              
 );
 
 CREATE TABLE games(
@@ -33,7 +36,10 @@ CREATE TABLE games(
         start_time              time            NOT NULL,
         start_date              date            NOT NULL,
         organizer               varchar(32)     NOT NULL,
-        number_of_players       int             NOT NULL
+        number_of_players       int             NOT NULL,
+        
+CONSTRAINT       PK_game_id             PRIMARY KEY (game_id)              
+
 );
 
 CREATE TABLE matches(
@@ -42,32 +48,43 @@ CREATE TABLE matches(
         start_time              time            NOT NULL,
         start_date              date            NOT NULL,
         game_title              varchar(64)     NOT NULL,
-        team_id                 varchar(32)     NOT NULL,
-        number_of_players       int             NOT NULL
+        team_id                 serial          NOT NULL,
+        number_of_players       int             NOT NULL,
+        
+CONSTRAINT       PK_match_id     PRIMARY KEY (tournament_id, game_id)              
+        
 );
 
-CREATE TABLE teams(
+CREATE TABLE team(
         tournament_id           serial          NOT NULL,
         game_id                 serial          NOT NULL,
         team_name               varchar(64)     NOT NULL,
-        list_of_players         varchar(64)     NOT NULL
+        team_id                 serial          NOT NULL,
+        list_of_players         varchar(64)     NOT NULL,
+        
+CONSTRAINT       PK_team_id     PRIMARY KEY (team_id , team_name)              
+
 
 );
 
-
-
-
-
-
-
-
-
+                                                                -- INSERT STATEMENTS
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 
 
-CONSTRAINTS   ()                  ; 
+                                                                -- SELECT STATEMENTS
+
+SELECT * FROM users;
+SELECT * FROM tournaments;
+SELECT * FROM games;
+SELECT * FROM matches;
+SELECT * FROM team;
+
+                                                                        --MAY NEED 
+--ALTER TABLE 
+--      constraint  name                constraint-type         columns  value        
+--ADD CONSTRAINT pk_person_address    PRIMARY KEY             (person_id,address_id);
 
 ROLLBACK;
 --COMMIT TRANSACTION;
