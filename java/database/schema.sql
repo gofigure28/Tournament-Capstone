3cd18d73ad5260afb0f7616f570514044c12c6af
Robert <robertlewiscook@gmail.com>
2021-03-05 13:03:59 -0500

    tables created, working on constraints

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -18,8 +18,56 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+CREATE TABLE tournaments(
+        tournament_id           serial          NOT NULL,
+        start_time              time            NOT NULL,
+        start_date              date            NOT NULL,
+        number_of_participants  int             NOT NULL,
+        team_id                 varchar(32)     NOT NULL,
+        ranking_of_teams        int             NOT NULL,
+        match_id                int             NOT NULL
+);
+
+CREATE TABLE games(
+        game_id                 serial          NOT NULL,
+        start_time              time            NOT NULL,
+        start_date              date            NOT NULL,
+        organizer               varchar(32)     NOT NULL,
+        number_of_players       int             NOT NULL
+);
+
+CREATE TABLE matches(
+        tournament_id           serial          NOT NULL,
+        game_id                 serial          NOT NULL,
+        start_time              time            NOT NULL,
+        start_date              date            NOT NULL,
+        game_title              varchar(64)     NOT NULL,
+        team_id                 varchar(32)     NOT NULL,
+        number_of_players       int             NOT NULL
+);
+
+CREATE TABLE teams(
+        tournament_id           serial          NOT NULL,
+        game_id                 serial          NOT NULL,
+        team_name               varchar(64)     NOT NULL,
+        list_of_players         varchar(64)     NOT NULL
+
+);
+
+
+
+
+
+
+
+
+
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 
-COMMIT TRANSACTION;
+
+CONSTRAINTS   ()                  ; 
+
+ROLLBACK;
+--COMMIT TRANSACTION;
