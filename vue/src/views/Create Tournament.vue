f86df9804a17fca9f0b2f37ff981dcdb7b99fe72
Robert <robertlewiscook@gmail.com>
2021-03-05 17:21:39 -0500

    working on creating a tournament

---
+++ vue/src/views/Create Tournament.vue
@@ -0,0 +1,50 @@
+
+                            /*Tournament needs
+
+    make sure the user is authorized to create a new tournament 
+
+    user need to be able to name the tournament
+
+    user needs to specify how many participants are in the tournament ??
+            - initiate a cut off for sign up afterwards the sign up is locked (must have)
+            - organizer has the option to leave sign ups open also (optional)
+
+            sign ups - allow users to request a spot in the tournament as a team or individual
+
+            */
+
+
+<template>
+  <h1>Create New Tounament</h1>
+  <p>The name of the tournament</p>
+</template>
+
+
+
+
+
+
+
+
+
+
+<script>
+export default {
+
+
+
+
+};
+</script>
+
+
+
+
+
+
+
+
+
+
+<style>
+</style>
