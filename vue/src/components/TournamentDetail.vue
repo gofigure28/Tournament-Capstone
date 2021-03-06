7cbdcfd6a0b2ff1488347badfb96b95aaead92b2
Robert <robertlewiscook@gmail.com>
2021-03-06 14:42:36 -0500

    some work done on creating a tournament

---
+++ vue/src/components/TournamentDetail.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Create New Tounament</h1>
+
+</template>
+
+
+<script>
+
+export default {
+    name: "Tournament-Detail",
+    components: { },
+    data() {
+        return{
+        tournament: {
+            tournament_id: "",
+            start_time: "",
+            start_date: "",
+            number_of_participants: "",
+            team_id: "",
+            ranking_of_teams: "",
+            match_id: ""
+            }
+        }
+
+        };
+    }
+
+};
+</script>
+
+
+
+
+
+<style>
+</style>
--- vue/src/views/Create Tournament.vue
+++ vue/src/views/Create Tournament.vue
@@ -1,23 +1,6 @@
 
-                            /*Tournament needs
-
-    make sure the user is authorized to create a new tournament 
-
-    user need to be able to name the tournament
-
-    user needs to specify how many participants are in the tournament ??
-            - initiate a cut off for sign up afterwards the sign up is locked (must have)
-            - organizer has the option to leave sign ups open also (optional)
-
-            sign ups - allow users to request a spot in the tournament as a team or individual
-
-            */
-
 
 <template>
-  <h1>Create New Tounament</h1>
-  <p>The name of the tournament</p>
-
 
 
 </template>
@@ -26,31 +9,13 @@
 
 
 
-
-
-
-
-
 <script>
+import Tournament from '@/components/TournamentDetail';
+
 export default {
-    data() {
-        return {
-            tournament: [
-                name: 'tournament name',
-                date: 'date',
-                time: 'time',
-                participants[
-
-                ]
-
-
-
-            ]
-        }
+    components: {
+        TournamentDetail
     }
-
-
-
 };
 </script>
 
