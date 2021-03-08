673b86c43a05c76b56d1f11ceae4f068fcf7678a
Kevin Kwan <Kevinkwan1997@gmail.com>
2021-03-08 11:11:30 -0500

    lord help us

--- java/src/main/java/com/techelevator/controller/TournamentController.java
+++ java/src/main/java/com/techelevator/controller/TournamentController.java
@@ -1,6 +1,7 @@
 package com.techelevator.controller;
 
 import com.techelevator.model.Tournament;
+import com.techelevator.dao.TournamentDAO;
 import com.techelevator.model.Match;
 
 import org.springframework.http.HttpEntity;
@@ -8,12 +9,21 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
-public class TournamentServices {
+
+@RestController
+@CrossOrigin
+public class TournamentController {
 
 	public String authToken = "";
+	
+	private TournamentDAO tournamentDAO;
 	
 	public void setAuthToken(String authToken) {
 		this.authToken = authToken;
@@ -22,6 +32,8 @@ public class TournamentServices {
 	private final String BASE_URL = "http://localhost:8080/";
 	private final RestTemplate restTemplate = new RestTemplate();
 	
+	
+	@RequestMapping(value ="/tournaments", method = RequestMethod.POST)
 	public Tournament makeTournament() {
 		Tournament tournament = null;
 		try {
@@ -32,7 +44,13 @@ public class TournamentServices {
 		return tournament;
 	}
 	
-	private HttpEntity makeAuthEntity() {
+	@RequestMapping(value ="/alltournaments", method = RequestMethod.GET)
+	public Tournament getTournament() {
+		Match fakeMatch = new Match(1, 2, "w", "o", "w", "w");
+		return new Tournament(1, 2, fakeMatch, "fe", 3);
+	}
+	
+	private HttpEntity makeAuthEntity() {	
 		HttpHeaders headers = new HttpHeaders();
 		headers.setBearerAuth(authToken);
 		HttpEntity entity = new HttpEntity<>(headers);
--- java/src/main/java/com/techelevator/dao/TournamentSqlDAO.java
+++ java/src/main/java/com/techelevator/dao/TournamentSqlDAO.java
@@ -9,15 +9,23 @@ import com.techelevator.model.Tournament;
 public class TournamentSqlDAO implements TournamentDAO {
 
 		private JdbcTemplate jdbcTemplate;
-		
 		private TournamentSqlDAO(JdbcTemplate jdbcTemplate) {
 			this.jdbcTemplate = jdbcTemplate;
 		}
 	@Override
 	public Tournament makeTournament(String name, String gameID, String startTime) {
 		String query = "insert into tournanments (name, game_ID, start_time) values(?, ?, ?)";
+		SqlRowSet result = jdbcTemplate.queryForRowSet(query);
 		
 		return null;
 	}
-
+	
+	/*private Tournament mapRow(SqlRowSet rw) {
+		
+		Tournament tournament = new Tournament();
+		
+	}*/
+	
+	
+	
 }
