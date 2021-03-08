496dfeb9018f24ee08be1b8843efda1d724b6c72
Stefan Friend <stefanpfriend@gmail.com>
2021-03-08 10:06:33 -0500

    styling is not doing anything apparently

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,3 +7,49 @@
     <router-view />
   </div>
 </template>
+
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+}
+.container {
+  width: 100%;
+  max-width: 1100px;
+  margin: 0 auto;
+}
+.loading {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.routerView {
+  width: 79%;
+  position: absolute;
+  right: 0;
+}
+.btn {
+  display: inline-block;
+  font-weight: 400;
+  color: #212529;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid transparent;
+  padding: .375rem .75rem;
+  margin-right: 10px;
+  margin-bottom: 5px;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: .25rem;
+  cursor: pointer;
+}
+</style>
