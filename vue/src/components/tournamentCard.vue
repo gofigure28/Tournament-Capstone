c443ac7e6af2574b4b2dd532862724627e29c657
Stefan Friend <stefanpfriend@gmail.com>
2021-03-11 12:16:36 -0500

    tournament card started

---
+++ vue/src/components/tournamentCard.vue
@@ -0,0 +1,30 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'tournament-card',
+    components: { },
+    data() {
+        return{
+        tournament: {
+            tournament_name: "",
+            tournament_id: "",
+            start_time: "",
+            start_date: "",
+            number_of_participants: "",
+            team_id: "",
+            ranking_of_teams: "",
+            match_id: ""
+            }
+        }
+
+    methods: {
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue/src/views/ViewAllTournaments.vue
+++ vue/src/views/ViewAllTournaments.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <h1>View all Tournaments</h1>
+  <div class="tournament-container">
+    <tournament-card v-bind:tournament="tournament in $store.state.tournaments" v-bind:key="tournament_id" />
+    
+  </div>
+</template>
+
+
+
+
+
+<script>
+import tournamentCard from './tournamentCard.vue';
+//import tournamentCard from '@/components/TournamentCard.vue';
+
+export default {
+  components: { tournamentCard },};
+
+</script>
+
+
+
+
+
+<style>
+</style>
+
+
+/*
+<h1>View all Tournaments</h1>
     <p>Tournament 1</p>
     <select name="" id="">
       
@@ -36,21 +63,4 @@
       <option value="Tournament">something</option>
       <option value="Tournament">something</option>
     </select>
-  </div>
-</template>
-
-
-
-
-
-<script>
-export default {};
-
-</script>
-
-
-
-
-
-<style>
-</style>
+    */
