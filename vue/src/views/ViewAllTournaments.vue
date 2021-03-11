ae8ef0df56f8cfc74676018557faa2e65cda0f27
Robert <robertlewiscook@gmail.com>
2021-03-10 21:11:34 -0500

    added the drop down for view all tournaments

--- vue/src/views/ViewAllTournaments.vue
+++ vue/src/views/ViewAllTournaments.vue
@@ -1,13 +1,55 @@
 <template>
-  <h1>View all Tournaments</h1>
+  <div>
+    <h1>View all Tournaments</h1>
+    <p>Tournament 1</p>
+    <select name="" id="">
+      
+      <option value="Tournament">Number of participants</option>
+      <option value="Tournament">Teams</option>
+      <option value="Tournament">something</option>
+      <option value="Tournament">something</option>
+    </select>
+
+    <p>Tournament 2</p>
+    <select name="" id="">
+     
+      <option value="Tournament">Number of participants</option>
+      <option value="Tournament">Teams</option>
+      <option value="Tournament">something</option>
+      <option value="Tournament">something</option>
+    </select>
+
+<p>Tournament 3</p>
+    <select name="" id="">
+      
+      <option value="Tournament">Number of participants</option>
+      <option value="Tournament">Teams</option>
+      <option value="Tournament">something</option>
+      <option value="Tournament">something</option>
+    </select>
+
+<p>Torurnament 4</p>
+    <select name="" id="">
+     
+      <option value="Tournament">Number of participants</option>
+      <option value="Tournament">Teams</option>
+      <option value="Tournament">something</option>
+      <option value="Tournament">something</option>
+    </select>
+  </div>
 </template>
 
-<script>
-export default {
 
-}
+
+
+
+<script>
+export default {};
 </script>
 
-<style>
 
+
+
+
+<style>
 </style>
