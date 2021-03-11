31ff32d4779c3004c1daf906cb4b982393b08e3c
Robert <robertlewiscook@gmail.com>
2021-03-11 13:49:48 -0500

    profile page sketched out

---
+++ vue/src/components/PlayerDetail.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Player Profile</h1>
+
+</template>
+
+
+<script>
+
+export default {
+    name: "Player-Detail",
+    components: { },
+    data() {
+        return{
+        player: {
+            tournament_name: "",
+            tournament_id: "",
+            start_time: "",
+            start_date: "",
+            number_of_participants: "",
+            team_id: "",
+            ranking_of_teams: "",
+            match_id: ""
+            }
+        }
+
+        },
+    };
+
+
+</script>
+
+
+
+
+.
+
+
+<style>
+</style>
