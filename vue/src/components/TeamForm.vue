70c5abc8cfde37053578d2fc7f1625c3249c6217
Stefan Friend <stefanpfriend@gmail.com>
2021-03-15 16:19:42 -0400

    team and player things

--- vue/src/components/TeamForm.vue
+++ vue/src/components/TeamForm.vue
@@ -1,7 +1,12 @@
 
 <template>
   <div class="dropdown">
-    <input v-model.trim="inputValue" class="dropdown-input" type="text" placeholder="Find country" />
+    <input
+      v-model.trim="inputValue"
+      class="dropdown-input"
+      type="text"
+      placeholder="Select Player"
+    />
     <div class="dropdown-list"></div>
   </div>
 </template>
@@ -12,9 +17,9 @@ export default {
   data () {
     return {
       inputValue: '',
-      itemList: [],
+      userList: [],
       apiLoaded: false,
-      apiUrl: 'https://restcountries.eu/rest/v2/all?fields=name;flag'
+      apiUrl: '../components/AllUsers'
     }
   },
   mounted () {
@@ -23,18 +28,20 @@ export default {
   methods: {
     getList () {
       axios.get(this.apiUrl).then( response => {
-        this.itemList = response.data
+        this.userList = response.data
         this.apiLoaded = true
       })
     },
      createTeam() {
           {
-              teamID: Number(this.$route.params.teamID),
-              teamName: this.teamname.title,
+              teamID: Number(this.$route.params.teamID);
+              teamName: this.teamname.title;
               addPlayer() {
-                  //input new player
-                  //dropdown of existing players
-
+                this.$store.commit('SET_USER', this.user);
+                this.userList = {
+                  users: ''};
+                this.$router.push({path: 'createTeam'})
+                 
               }
 
 
@@ -56,13 +63,13 @@ export default {
 </script>
 
 <style>
-.dropdown{
+.dropdown {
   position: relative;
   width: 100%;
   max-width: 400px;
   margin: 0 auto;
 }
-.dropdown-input{
+.dropdown-input {
   width: 100%;
   padding: 10px 16px;
   border: 1px solid transparent;
@@ -71,11 +78,11 @@ export default {
   outline: none;
   border-radius: 8px;
 }
-.dropdown-input:focus{
+.dropdown-input:focus {
   background: #fff;
   border-color: #e2e8f0;
 }
-.dropdown-input::placeholder{
+.dropdown-input::placeholder {
   opacity: 0.7;
 }
 </style>
---
+++ vue/src/views/CreateTeam.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div>
+   <center><h1>Create A New Team</h1></center> 
+ <match-form/>
+
+     
+  </div>
+</template>
+ 
+
+
+<script>
+import matchForm from '../components/CreateTeam'
+
+export default {
+  components: {CreateTeam},
+
+  methods: {},
+};
+
+</script>
+<style>
+
+</style>
